557886bfd7aadced933f46e70bfc11f8434dd1b8
moylop260 <moylop260@vauxoo.com>
2014-11-13 21:10:09 -0600

    [REF] auto-fix conventions

--- exportsage50/wizard/exportsage50.py
+++ exportsage50/wizard/exportsage50.py
@@ -20,20 +20,16 @@
 ##############################################################################
 
 from osv import osv, fields
-import web
 import base64
-import tools
 from tools.translate import _
-from tools.misc import get_iso_codes
 import pooler
 from datetime import datetime
-import decimal_precision as dp
 
 
 class exportsage(osv.osv):
 
     """
-    Wizard 
+    Wizard
     """
     _name = "exportsage"
     _description = "Create imp file  to export  in sage50"
@@ -42,7 +38,7 @@ class exportsage(osv.osv):
         'data': fields.binary('File', readonly=True),
         'name': fields.char('Filename', 20, readonly=True),
         'format': fields.char('File Format', 10),
-        'state': fields.selection([('choose', 'choose'), # choose date
+        'state': fields.selection([('choose', 'choose'),  # choose date
                                    ('get', 'get')]),
         'invoice_ids': fields.many2many('account.invoice', 'sale_order_invoice_export_rel', 'order_id', 'invoice_id',
                                         'Invoices', required=True,
@@ -52,15 +48,14 @@ class exportsage(osv.osv):
     _defaults = {
         'state': lambda *a: 'choose',
     }
+
     def act_cancel(self, cr, uid, ids, context=None):
         #self.unlink(cr, uid, ids, context)
-        return {'type':'ir.actions.act_window_close' }
-
+        return {'type': 'ir.actions.act_window_close'}
 
     def act_destroy(self, *args):
-        return {'type':'ir.actions.act_window_close' }   
+        return {'type': 'ir.actions.act_window_close'}
 
- 
     def create_report(self, cr, uid, ids, context=None):
         if context == None:
             context = {}
@@ -68,17 +63,19 @@ class exportsage(osv.osv):
         data = self.read(cr, uid, ids, [], context=context)[0]
 
         if not data['invoice_ids']:
-            raise osv.except_osv(_('Error'), _('You have to select at least 1 Invoice. And try again'))
+            raise osv.except_osv(
+                _('Error'), _('You have to select at least 1 Invoice. And try again'))
 
-        output = '<Version>''\n' + '"12001"' + ',' + '"1"''\n' + '</Version>\n\n'
-        #Faire le traitement des autres lignes dans les lignes de factures
+        output = '<Version>''\n' + '"12001"' + \
+            ',' + '"1"''\n' + '</Version>\n\n'
+        # Faire le traitement des autres lignes dans les lignes de factures
         pool = pooler.get_pool(cr.dbname)
         line_obj = pool.get('account.invoice')
 
         for line in line_obj.browse(cr, uid, data['invoice_ids'], context):
             # tag de debut pour les lignes de factures
             output += '<SalInvoice>''\n'
-            #informations sur le client
+            # informations sur le client
             costumer_name = line.partner_id.name
             oneTimefield = ""
             contact_name = line.partner_id.address[0].name or ""
@@ -97,32 +94,36 @@ class exportsage(osv.osv):
                       city, province_state, zip_code, country, phone1, mobile, fax, email
                       ]
             costumer = ','.join(['"%s"' % field for field in fields])
-            #print costumer
-            #exit(0)
+            # print costumer
+            # exit(0)
             output += costumer.encode('UTF-8') + '\n'
-            #informations sur la facture
+            # informations sur la facture
             no_of_details = str(len(line.invoice_line))
             order_no = ""
             # Invoice number (Max 20 chars)
             invoice_no = str(line.number)
             # date de la facture
             if line.date_invoice:
-                entry_date = datetime.strptime(line.date_invoice, "%Y-%m-%d").strftime('%m-%d-%Y')  # date format : mm-dd-yyyy
+                entry_date = datetime.strptime(
+                    line.date_invoice, "%Y-%m-%d").strftime('%m-%d-%Y')  # date format : mm-dd-yyyy
             else:
                 entry_date = ""
             # Informations sur le type de paiement (between 0 and 3)
             # 0 = pay later , 1 = cash , 2 = cheque , 3 = credit card
             # Selectionner le dernier paiement
             list_id = []
-            # Paid by source (20 Chars) : Blank- pay later and cash Cheque number or credit card
+            # Paid by source (20 Chars) : Blank- pay later and cash Cheque
+            # number or credit card
             paid_by_source = ""
             if line.payment_ids:
                 for oneId in line.payment_ids:
                     list_id.append(oneId.id)
                 lastId = max(list_id)
-                # acceder à partir du dernier paiement à l'objet account_move_line
+                # acceder à partir du dernier paiement à l'objet
+                # account_move_line
                 account_move_line_obj = self.pool.get('account.move.line')
-                account_move_line = account_move_line_obj.browse(cr, uid, lastId, context=context)
+                account_move_line = account_move_line_obj.browse(
+                    cr, uid, lastId, context=context)
                 paiement_type = account_move_line.journal_id.type
                 if paiement_type == 'cash':
                     paid_by_type = str(1)
@@ -130,22 +131,24 @@ class exportsage(osv.osv):
                 elif paiement_type == 'bank':
                     paid_by_type = str(2)
                 else:
-                    paid_by_type = str(0) # default value 0 = pay later
+                    paid_by_type = str(0)  # default value 0 = pay later
 
             else:
-                paid_by_type = str(0) # default value 0 = pay later
+                paid_by_type = str(0)  # default value 0 = pay later
             total_amount = str(line.amount_total) or ""
             freight_amount = "0.0"
             fields_sale_invoice = [no_of_details, order_no, invoice_no, entry_date, paid_by_type,
-                                  paid_by_source, total_amount, freight_amount,
-                                  ]
-            sale_invoice = ','.join(['"%s"' % field_sale_invoice for field_sale_invoice in fields_sale_invoice])
+                                   paid_by_source, total_amount, freight_amount,
+                                   ]
+            sale_invoice = ','.join(
+                ['"%s"' % field_sale_invoice for field_sale_invoice in fields_sale_invoice])
             #sale_invoice = '"' + no_of_details + '"' + ',"' + order_no + '"' + ',"' + invoice_no + '"' + ',"' + entry_date + '"' + ',"' + paid_by_type + '"' + ',"' + paid_by_source + '"' + ',"' + total_amount + '"' + ',"' + freight_amount + '"'
             output += sale_invoice.encode('UTF-8') + '\n'
             product_line_invoice_with_taxe = ""
-            #Sale invoice detail lines
-            account_invoice_line_obj =self.pool.get('account.invoice.line')
-            product_ids = account_invoice_line_obj.search(cr, uid, [('invoice_id','=',line.id)])
+            # Sale invoice detail lines
+            account_invoice_line_obj = self.pool.get('account.invoice.line')
+            product_ids = account_invoice_line_obj.search(
+                cr, uid, [('invoice_id', '=', line.id)])
 
             if product_ids:
                 for product in account_invoice_line_obj.browse(cr, uid, product_ids):
@@ -153,32 +156,39 @@ class exportsage(osv.osv):
                     quantity = str(product.quantity)
                     price = str(product.price_unit)
                     amount = product.quantity * product.price_unit
-                    amount = str(round(amount, self.pool.get('decimal.precision').precision_get(cr, uid, 'Account')))
-                    fields_one_product_invoice = [item_number, quantity, price, amount]
-                    one_product_invoice = ','.join(['"%s"' % field_one_product_invoice for field_one_product_invoice in fields_one_product_invoice])
+                    amount = str(round(
+                        amount, self.pool.get('decimal.precision').precision_get(cr, uid, 'Account')))
+                    fields_one_product_invoice = [
+                        item_number, quantity, price, amount]
+                    one_product_invoice = ','.join(
+                        ['"%s"' % field_one_product_invoice for field_one_product_invoice in fields_one_product_invoice])
                     #one_product_invoice = '"' + item_number + '"' + ',"' + quantity + '"' + ',"' + price + '"' + ',"' + amount + '"'
                     one_product_invoice = one_product_invoice.encode('UTF-8')
                     tax_product_line = ""
                     # tax information pour chaque produit
                     if product.invoice_line_tax_id:
                         for one_taxe in product.invoice_line_tax_id:
-                            tax_name = one_taxe.description # or one_taxe.description or one_taxe.name
+                            # or one_taxe.description or one_taxe.name
+                            tax_name = one_taxe.description
                             if one_taxe.price_include:
-                                tax_included = str(1) # 1=yes,0=No
+                                tax_included = str(1)  # 1=yes,0=No
                             else:
-                                tax_included = str(0) # 1=yes,0=No
-                            tax_refundable = str(1) # 1=yes,0=No
+                                tax_included = str(0)  # 1=yes,0=No
+                            tax_refundable = str(1)  # 1=yes,0=No
                             tax_rate = str(one_taxe.amount)
                             tax_amount = str(one_taxe.amount)
                             fields_tax_product_line = [tax_name, tax_included, tax_refundable, tax_rate, tax_amount,
                                                        ]
-                            tax_product_line = ',' + ','.join(['"%s"' % field_tax_product_line for field_tax_product_line in fields_tax_product_line])
+                            tax_product_line = ',' + \
+                                ','.join(
+                                    ['"%s"' % field_tax_product_line for field_tax_product_line in fields_tax_product_line])
                             #tax_product_line += ',"' + tax_name + '"' + ',"' + tax_included + '"' + ',"' + tax_refundable + '"' + ',"' + tax_rate + '"' + ',"' + tax_amount + '"'
 
                         #tax_product_line = tax_product_line[:-1]
                         tax_product_line = tax_product_line.encode('UTF-8')
-                    product_line_invoice_with_taxe += one_product_invoice + tax_product_line + '\n'
-                #print product_line_invoice_with_taxe , exit(0)
+                    product_line_invoice_with_taxe += one_product_invoice + \
+                        tax_product_line + '\n'
+                # print product_line_invoice_with_taxe , exit(0)
 
                 output += product_line_invoice_with_taxe
             # tag de fin  pour les lignes de factures
@@ -189,8 +199,9 @@ class exportsage(osv.osv):
         filename = 'export_to_sage50'
         this.name = "%s.%s" % (filename, this.format)
         out = base64.encodestring(output)
-        self.write(cr, uid, ids, {'state':'get', 'data':out, 'name':this.name, 'format' : this.format}, context=context)
-        
+        self.write(cr, uid, ids, {
+                   'state': 'get', 'data': out, 'name': this.name, 'format': this.format}, context=context)
+
 exportsage()
 
 # vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
